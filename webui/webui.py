0fd7c7e5b0d36c0105b0c185ef73f92386a4fceb
g0t3n <g0t3n@g0t3n.th3.r0ck>
2014-09-28 11:34:17 +0800

    base frame

---
+++ webui/webui.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+import string
+from urllib import unquote
+from json import dumps as jsondumps
+
+import tornado.ioloop
+import tornado.web
+
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+    def get(self):
+        self.write_error(404)
+    def post(self):
+        self.write_error(404)
+    def write_error(self, status_code, **kwargs):
+        if status_code == 404:
+            self.render('404.html')
+        else:
+            self.write('error:' + str(status_code))
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        raise NotImplemented,""
+        self.write('<html><body><form action="/login" method="post">'
+                   'Name: <input type="text" name="name">'
+                   '<input type="submit" value="Sign in">'
+                   '</form></body></html>')
+    def post(self):
+        raise NotImplemented,""
+        # 这里补充一个，获取用户输入
+        self.get_argument("name")
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/index")
+
+class MainHandler(tornado.web.RequestHandler):
+    #@tornado.web.authenticated
+    def get(self):
+        self.render('index.html', title= 'Weekly ~~', AdminDelTask="123")
+    def post(self):
+        pass
+
+class AddTask(tornado.web.RequestHandler):
+    def post(self):
+        self.write("lalalal")
+
+settings = {
+        "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+        # "xsrf_cookies": True,
+        'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+        'static_path':  os.path.join(os.path.dirname(__file__), 'static'),
+        # "login_url": "/login",
+        }
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+     # (r"/login", LoginHandler),
+    (r"/index*", MainHandler),
+    (r"/AddTask/*", AddTask),
+    (r".*", BaseHandler),
+    ], **settings)
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
